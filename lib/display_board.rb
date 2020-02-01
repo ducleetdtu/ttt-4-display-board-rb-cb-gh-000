c4b7e45f47f7996abe62ca7507f94bc58e5931d2
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 04:43:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(state)
-  puts " #{"" | state[0]} | #{state[1]} | #{state[2]}"
+  puts " #{" " | state[0]} | #{state[1]} | #{state[2]}"
   puts "-----------"
   puts " #{state[3]} | #{state[4]} | #{state[5]}"
   puts "-----------"
